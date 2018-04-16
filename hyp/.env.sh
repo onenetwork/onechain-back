7c8947bd0383c5830475523633cffa08b7595754
Greg Merrill <gmerrill@onenetwork.com>
2018-04-16 17:49:40 -0500

    hyperledger vagrant

---
+++ hyp/.env.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+export PS1="\[\e]0;\w\a\]\n\[\e[34m\]*\h* \[\e[33m\]\w\[\e[0m\]\n\$ "
+
+export PATH=$PATH:/vagrant/fabric-samples/bin
+
+echo
+echo
+echo "--- ONE Backchain Environment (Hyperledger) ---"
+
+alias e=vim
+
+export LS_COLORS='ow=01;36;40'
+
